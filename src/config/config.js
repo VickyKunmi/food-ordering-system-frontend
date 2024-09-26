1a60a5aa25bbef60210aac41c8c123a4ded12696
Victory Olamide Oyekunle <76197526+VickyKunmi@users.noreply.github.com>
2024-09-26 14:10:41 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 
 const config = {
     // url: "http://localhost:3000",
-    url: "https://chopexpressbackend-8zol.onrender.com/"
+    url: "https://chopexpressbackend-8zol.onrender.com"
   };
   
   export default config;
